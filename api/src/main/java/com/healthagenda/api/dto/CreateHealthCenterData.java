6b8ef49da63a3129a39e4a0c8ffafb405011a337
Matheus Pegorari <69256236+matheuspegorari@users.noreply.github.com>
2023-10-20 00:35:12 -0300

    changed how LocalTime is shown in Swagger-UI

--- api/src/main/java/com/healthagenda/api/dto/CreateHealthCenterData.java
+++ api/src/main/java/com/healthagenda/api/dto/CreateHealthCenterData.java
@@ -1,6 +1,8 @@
 package com.healthagenda.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.healthagenda.api.model.Employee;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -12,9 +14,14 @@ public record CreateHealthCenterData(
         @NotBlank String centerName,
         @Email
         String email,
+
+        @Schema(type = "String", pattern = "HH:mm:SS")
         @NotNull LocalTime openingTime,
+        @Schema(type = "String", pattern = "HH:mm:SS")
         @NotNull LocalTime closingTime,
         Long manager,
         @Valid CreateAddressData address
+
 ) {
+
 }
